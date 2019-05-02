75539aad2761cc9f09bb84aaa1e8cac4e15baf4f
tanlinlin <tanlinlin@xiaozhu.com>
2019-05-02 11:32:17 +0800

    readme

--- README.md
+++ README.md
@@ -1,34 +1,22 @@
 # vue-prerender-app1
 
-## Project setup
+vue prerender demo
+- 使用vue-cli3搭建项目，要使用customer直接装上想要的依赖，否则还需要之后手动安装
+- 使用插件：prerender-spa-plugin。安装命令: npm install --save prerender-spa-plugin；：Wq[官方文档](https://www.npmjs.com/package/prerender-spa-plugin).
+
+- 在新建的项目根目录下新建vue.config.js文件，详情请看vue.config.js文件
+- 修改mian.js如下
 ```
-npm install
+new Vue({
+  router,
+  store,
+  render: h => h(App),
+  mounted () {
+    document.dispatchEvent(new Event('render-event'))
+  }
+}).$mount('#app')
 ```
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+- 现在 npm run build,就能看到dist文件下增加了对应vue.config.js里面的routes的目录，目录下是一个html文件，是静态文件
 
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Run your unit tests
-```
-npm run test:unit
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+> 预渲染：构建阶段生成匹配预渲染路径的 html 文件（注意：每个需要预渲染的路由都有一个对应的 html）。构建出来的 html 文件已经有静态数据，需要ajax数据的部分未构建。
--- vue.config.js
+++ vue.config.js
@@ -4,23 +4,26 @@ const path = require('path');
 module.exports = {
     configureWebpack: config => {
         if (process.env.NODE_ENV !== 'production') return;
+        // 在生产环境下
         return {
             plugins: [
                 new PrerenderSPAPlugin({
-                    // 生成文件的路径，也可以与webpakc打包的一致。
-                    // 下面这句话非常重要！！！
-                    // 这个目录只能有一级，如果目录层次大于一级，在生成的时候不会有任何错误提示，在预渲染的时候只会卡着不动。
+                    //  Required - The path to the webpack-outputted app to prerender.
                     staticDir: path.join(__dirname,'dist'),
-                    // 对应自己的路由文件，比如a有参数，就需要写成 /a/param1。
-                    routes: ['/', '/aboutnew','/about'],
-                    // 这个很重要，如果没有配置这段，也不会进行预编译
+                    // Required - Routes to render.
+                    routes: ['/', '/aboutnew','/about','/jddkjd'],
+                    // The actual renderer to use. (Feel free to write your own)
+                    // Available renderers: https://github.com/Tribex/prerenderer/tree/master/renderers
                     renderer: new Renderer({
+                        // Optional - Any values you'd like your app to have access to via `window.injectProperty`.
                         inject: {
                             foo: 'bar'
                         },
-                        headless: false,
-                        // 在 main.js 中 document.dispatchEvent(new Event('render-event'))，两者的事件名称要对应上。
-                        renderAfterDocumentEvent: 'render-event'
+                        // Other puppeteer options.
+                        // (See here: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#puppeteerlaunchoptions)
+                        headless: false, // Display the browser window when rendering. Useful for debugging.
+                        // Optional - Wait to render until the specified element is detected using `document.querySelector`
+                        renderAfterDocumentEvent: 'render-event' 
                     })
                 }),
             ],
